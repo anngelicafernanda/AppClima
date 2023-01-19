ef8a8024a8267484af420cbfd2380985f9c20b31
anngelicafernanda <devanngelc@gmail.com>
2023-01-19 18:27:30 -0300

    Validación y lectura de datos del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,22 +1,41 @@
+import { useState } from "react";
 import useClima from "../hooks/useClima";
 
 const Formulario = () => {
+  const [alerta, setAlerta] = useState("");
+  const { busqueda, datosBusqueda, consultarClima } = useClima();
+  const { ciudad, pais } = busqueda;
 
-    const {busqueda, datosBusqueda} = useClima()
-    const {ciudad, pais} = busqueda
-    return (
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Todos los campos son obligatorios");
+      return;
+    }
+
+    consultarClima(busqueda)
+  };
+
+  return (
     <div className="contenedor">
-      <form>
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
         <div className="campo">
           <label htmlFor="ciudad">Ciudad</label>
-          <input value={ciudad} onChange={datosBusqueda} type="text" name="ciudad" id="ciudad" />
+          <input
+            value={ciudad}
+            onChange={datosBusqueda}
+            type="text"
+            name="ciudad"
+            id="ciudad"
+          />
         </div>
 
         <div className="campo">
           <label htmlFor="pais">País</label>
-        <select value={pais} onChange={datosBusqueda} name="pais" id="pais">
+          <select value={pais} onChange={datosBusqueda} name="pais" id="pais">
             <option value="">-- Seleccione un País --</option>
-            <option value="US">estados Unidos</option>
+            <option value="US">Estados Unidos</option>
             <option value="MX">México</option>
             <option value="AR">Argentina</option>
             <option value="CO">Colombia</option>
@@ -25,8 +44,7 @@ const Formulario = () => {
             <option value="PE">Perú</option>
             <option value="VE">Venezuela</option>
             <option value="CL">Chile</option>
-
-        </select>
+          </select>
         </div>
         <input type="submit" value="Consultar Clima" />
       </form>
