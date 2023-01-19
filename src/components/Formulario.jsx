d82cb49e32c27cf72eb073c5184b782af93eabeb
anngelicafernanda <devanngelc@gmail.com>
2023-01-19 18:01:00 -0300

    Creando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,32 @@
-
-
 const Formulario = () => {
   return (
-    <div>
-      Formulario
-    </div>
-  )
-}
+    <div className="contenedor">
+      <form>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input type="text" name="ciudad" id="ciudad" />
+        </div>
 
-export default Formulario
+        <div className="campo">
+          <label htmlFor="pais">País</label>
+        <select name="pais" id="pais">
+            <option value="">-- Seleccione un País --</option>
+            <option value="US">estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+            <option value="VE">Venezuela</option>
+            <option value="CL">Chile</option>
+
+        </select>
+        </div>
+        <input type="submit" value="Consultar Clima" />
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
